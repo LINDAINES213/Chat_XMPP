cbf17dbb44aeefa25808ccdd980c724088a33c59
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2024-07-29 00:24:32 -0600

    Initial commit

---
+++ src/test/java/com/proyecto1redes/demoproyecto1/Demoproyecto1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto1redes.demoproyecto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoproyecto1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

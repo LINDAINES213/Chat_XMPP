d79c0b2efa34e952d0b8b1bb0525c1812de8d0d1
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2024-08-01 15:02:08 -0600

    correccion certificados

--- src/main/java/com/proyecto1redes/demoproyecto1/XMPPConnection.java
+++ src/main/java/com/proyecto1redes/demoproyecto1/XMPPConnection.java
@@ -8,43 +8,23 @@ import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
 import org.springframework.stereotype.Service;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
+//import javax.net.ssl.SSLContext;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
-import java.security.cert.X509Certificate;
 
 @Service
 public class XMPPConnection {
 
     public AbstractXMPPConnection connect(String username, String password) throws GeneralSecurityException, IOException, XMPPException, SmackException, InterruptedException {
-        SSLContext sslContext = SSLContext.getInstance("TLS");
-        TrustManager[] trustManagers = new TrustManager[]{
-                new X509TrustManager() {
-                    @Override
-                    public void checkClientTrusted(X509Certificate[] chain, String authType) {
-                    }
+        //SSLContext sslContext = SSLContext.getInstance("TLS");
 
-                    @Override
-                    public void checkServerTrusted(X509Certificate[] chain, String authType) {
-                    }
-
-                    @Override
-                    public X509Certificate[] getAcceptedIssuers() {
-                        return new X509Certificate[0];
-                    }
-                }
-        };
-        sslContext.init(null, trustManagers, new java.security.SecureRandom());
-
-        @SuppressWarnings("deprecation")
+        //@SuppressWarnings("deprecation")
         XMPPTCPConnectionConfiguration config = XMPPTCPConnectionConfiguration.builder()
                 .setUsernameAndPassword(username, password)
                 .setXmppDomain("alumchat.lol")
                 .setHost("alumchat.lol")
-                .setPort(5222)
-                .setCustomSSLContext(sslContext)
+                //.setPort(5222)
+                //.setCustomSSLContext(sslContext)
                 .setSecurityMode(ConnectionConfiguration.SecurityMode.disabled)
                 .build();
 

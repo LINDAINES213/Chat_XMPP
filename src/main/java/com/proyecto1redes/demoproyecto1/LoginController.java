83644415170caecba71d093b90c089dcda6c01b3
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2024-08-04 20:50:15 -0600

    buscar y agregar contacto

--- src/main/java/com/proyecto1redes/demoproyecto1/LoginController.java
+++ src/main/java/com/proyecto1redes/demoproyecto1/LoginController.java
@@ -3,6 +3,9 @@ package com.proyecto1redes.demoproyecto1;
 import org.jivesoftware.smack.AbstractXMPPConnection;
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.roster.Roster;
+import org.jivesoftware.smack.roster.RosterEntry;
+import org.jxmpp.jid.impl.JidCreate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.Set;
 
 @Controller
 public class LoginController {
@@ -30,13 +34,34 @@ public class LoginController {
     public String login(@RequestParam String username, @RequestParam String password, Model model) {
         try {
             connection = xmppConnection.connect(username, password);
+            Roster roster = Roster.getInstanceFor(connection);
+            Set<RosterEntry> entries = roster.getEntries();
             model.addAttribute("message", "Welcome " + connection.getUser());
+            model.addAttribute("userList", entries);
             return "loggedin";
         } catch (GeneralSecurityException | IOException | XMPPException | SmackException | InterruptedException e) {
             e.printStackTrace();
             model.addAttribute("error", "Failed to connect: " + e.getMessage());
             return "home";
         }
+    }
+
+    @SuppressWarnings("deprecation")
+    @PostMapping("/search")
+    public String search(@RequestParam String searchUsername, Model model) {
+        try {
+            if (connection != null && connection.isAuthenticated()) {
+                Roster roster = Roster.getInstanceFor(connection);
+                roster.createEntry(JidCreate.bareFrom(searchUsername + "@alumchat.lol"), searchUsername, new String[]{});
+                Set<RosterEntry> entries = roster.getEntries();
+                model.addAttribute("message", "User " + searchUsername + " added to contacts.");
+                model.addAttribute("userList", entries);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("error", "Failed to add user: " + e.getMessage());
+        }
+        return "loggedin";
     }
 
     @PostMapping("/")

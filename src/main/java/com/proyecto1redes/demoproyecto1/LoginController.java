80364b88b49ceec805eba2a494cc82a4a090d0fd
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2024-08-06 21:07:36 -0600

    creacion cuentas

--- src/main/java/com/proyecto1redes/demoproyecto1/LoginController.java
+++ src/main/java/com/proyecto1redes/demoproyecto1/LoginController.java
@@ -3,8 +3,10 @@ package com.proyecto1redes.demoproyecto1;
 import org.jivesoftware.smack.AbstractXMPPConnection;
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Presence;
 import org.jivesoftware.smack.roster.Roster;
 import org.jivesoftware.smack.roster.RosterEntry;
+import org.jxmpp.jid.BareJid;
 import org.jxmpp.jid.impl.JidCreate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,8 +15,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @Controller
@@ -30,6 +36,24 @@ public class LoginController {
         return "home";
     }
 
+    @GetMapping("/signup")
+    public String signup() {
+        return "signUp";
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestParam String username, /*@RequestParam String name*/ @RequestParam String password, Model model) {
+        try {
+            xmppConnection.registerUser(username, password);
+            model.addAttribute("message", "User registered successfully. Please log in.");
+            return "home";
+        } catch (XMPPException | SmackException | IOException | InterruptedException e) {
+            e.printStackTrace();
+            model.addAttribute("error", "Failed to register: " + e.getMessage());
+            return "signUp";
+        }
+    }
+
     @PostMapping("/login")
     public String login(@RequestParam String username, @RequestParam String password, Model model) {
         try {
@@ -38,6 +62,21 @@ public class LoginController {
             Set<RosterEntry> entries = roster.getEntries();
             model.addAttribute("message", "Welcome " + connection.getUser());
             model.addAttribute("userList", entries);
+            
+            Map<String, List<Presence>> presencesMap = new HashMap<>();
+            for (RosterEntry entry : entries) {
+                BareJid entryBareJid = entry.getJid().asBareJid();
+                List<Presence> presences = roster.getAllPresences(entryBareJid);
+                presencesMap.put(entryBareJid.toString(), presences);
+
+                // Imprimir la información de presencia en la consola
+                System.out.println("Presences for " + entryBareJid + ":");
+                for (Presence presence : presences) {
+                    System.out.println("  Mode: " + presence.getMode());
+                    System.out.println("  Status: " + presence.getStatus());
+                    System.out.println("  Type: " + presence.getType());
+                }
+            }
             return "loggedin";
         } catch (GeneralSecurityException | IOException | XMPPException | SmackException | InterruptedException e) {
             e.printStackTrace();
@@ -51,7 +90,8 @@ public class LoginController {
         try {
             if (connection != null && connection.isAuthenticated()) {
                 Roster roster = Roster.getInstanceFor(connection);
-                roster.createItemAndRequestSubscription(JidCreate.bareFrom(searchUsername + "@alumchat.lol"), searchUsername, new String[]{});
+                BareJid bareJid = JidCreate.bareFrom(searchUsername + "@alumchat.lol");
+                roster.createItemAndRequestSubscription(bareJid, searchUsername, new String[]{});
                 Set<RosterEntry> entries = roster.getEntries();
                 model.addAttribute("message", "User " + searchUsername + " added to contacts.");
                 model.addAttribute("userList", entries);
@@ -62,6 +102,7 @@ public class LoginController {
         }
         return "loggedin";
     }
+
 
     @PostMapping("/")
     public String logout(Model model) throws SmackException.NotConnectedException, InterruptedException {
--- src/main/java/com/proyecto1redes/demoproyecto1/XMPPConnection.java
+++ src/main/java/com/proyecto1redes/demoproyecto1/XMPPConnection.java
@@ -6,6 +6,8 @@ import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
+import org.jivesoftware.smackx.iqregister.AccountManager;
+import org.jxmpp.jid.parts.Localpart;
 import org.springframework.stereotype.Service;
 
 //import javax.net.ssl.SSLContext;
@@ -31,5 +33,22 @@ public class XMPPConnection {
         connection.connect();
         connection.login();
         return connection;
+    }
+
+    public void registerUser(String username, String password) throws XMPPException, SmackException, IOException, InterruptedException {
+        XMPPTCPConnectionConfiguration config = XMPPTCPConnectionConfiguration.builder()
+                .setXmppDomain("alumchat.lol")
+                .setHost("alumchat.lol")
+                .setSecurityMode(ConnectionConfiguration.SecurityMode.disabled)
+                .build();
+
+        AbstractXMPPConnection connection = new XMPPTCPConnection(config);
+        connection.connect();
+
+        AccountManager accountManager = AccountManager.getInstance(connection);
+        accountManager.sensitiveOperationOverInsecureConnection(true);
+        accountManager.createAccount(Localpart.from(username), password);
+
+        connection.disconnect();
     }
 }
